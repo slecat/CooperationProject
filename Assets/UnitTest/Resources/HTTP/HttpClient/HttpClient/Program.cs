645f4dae5dbff0f43cb3cf9aaa6606eae7dfde99
slecat <1528449507@qq.com>
2024-09-07 18:05:01 +0800

    HTTPServer_1 OK

---
+++ Assets/UnitTest/Resources/HTTP/HttpClient/HttpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleHttpClient;
+
+Console.WriteLine("Hello, World!");
+
+string url = "http://localhost:8888/";
+//将接口传入，这个HttpUitls的类，有兴趣可以研究下，也可以直接用就可以，不用管如何实现。
+string getJson = HttpUitls.Get(url);
+Console.Write(getJson);
+//JsonConvert.SerializeObject(listjson);
+//JSON.parse(json);
+Console.ReadKey();
---
+++ Assets/UnitTest/Resources/HTTP/HttpServer/HttpServer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleHttpServer;
+
+Console.WriteLine("Hello, World!");
+HttpServer httpServer;
+if (args.GetLength(0) > 0)
+{
+    httpServer = new MyHttpServer(Convert.ToInt16(args[0]));
+}
+else
+{
+    httpServer = new MyHttpServer(8888);
+}
+Thread thread = new Thread(new ThreadStart(httpServer.listen));
+thread.Start();
